08d4a25756a19169d91316058b6a714c008d3c13
Martin <fridaymartin500@gmail.com>
2024-05-17 15:42:44 +0100

    2nd Commit

--- app/components/LoadingPage.js
+++ app/components/LoadingPage.js
@@ -2,16 +2,14 @@ import React from "react";
 
 const LoadingPage = () => {
   return (
-    <div>
-      <main>
-        <div className="justify-center h-full w-full text-center text-5xl">
-          <h2 className="text-center items-center flex h-full w-full">
-            Loading...
-          </h2>
-          <p>Won't be long, wait a while, please.</p>
-        </div>
-      </main>
-    </div>
+    <main>
+      <div className="justify-center h-full w-full text-center text-5xl">
+        <h2 className="text-center items-center flex h-full w-full">
+          Loading...
+        </h2>
+        <p>Won't be long, wait a while, please.</p>
+      </div>
+    </main>
   );
 };
 
