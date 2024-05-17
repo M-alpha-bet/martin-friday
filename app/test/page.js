1cbaef98e04a688b92388e72bc28aeafe9279778
Martin <fridaymartin500@gmail.com>
2024-05-17 16:35:43 +0100

    4th Commit

--- app/test/page.js
+++ app/test/page.js
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { Player } from "@lottiefiles/react-lottie-player";
-import styles from "../components/styles/Animation.module.css";
+import "../components/styles/Animation.css";
 
 const TestPage = () => {
   const text = "Hooge";
@@ -9,7 +9,7 @@ const TestPage = () => {
     <div className="h-full w-full text-center">
       <div className="h-full w-full justify-center flex py-44">
         <div>
-          <div className={styles.animate}>
+          <div className="animate">
             <Player
               src="https://lottie.host/77e98272-04f3-44c5-96b2-6fd9d2d5e2d4/IkHubDH17O.json"
               style={{ width: "350px", height: "350px" }}
